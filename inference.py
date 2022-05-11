60bfc7626a4bcf5e6c5b59b4c25b14ad4b043637
HanNayeoniee <piggutail79@gmail.com>
2022-05-11 12:32:19 +0000

    feat: elasticsearch

--- inference.py
+++ inference.py
@@ -20,7 +20,7 @@ from datasets import (
     load_from_disk,
     load_metric,
 )
-from retrieval import BM25, SparseRetrieval
+from retrieval import BM25, SparseRetrieval, ElasticRetrieval
 from trainer_qa import QuestionAnsweringTrainer
 from transformers import (
     AutoConfig,
@@ -107,10 +107,21 @@ def run_sparse_retrieval(
 ) -> DatasetDict:
 
     # Query에 맞는 Passage들을 Retrieval 합니다.
-    retriever = BM25(
-        tokenize_fn=tokenize_fn, data_path=data_path, context_path=context_path
-    )
-    retriever.get_sparse_embedding()
+    # Elasticsearch 사용하는 경우
+    if data_args.elastic:
+        retriever = ElasticRetrieval(data_args.index_name)
+    else:
+        # BM25 사용하는 경우
+        if data_args.bm25:
+            retriever = BM25(
+                tokenize_fn=tokenize_fn, data_path=data_path, context_path=context_path
+            )
+        # TF-IDF 사용하는 경우
+        else:
+            retriever = SparseRetrieval(
+                tokenize_fn=tokenize_fn, data_path=data_path, context_path=context_path
+            )
+        retriever.get_sparse_embedding()
 
     if data_args.use_faiss:
         retriever.build_faiss(num_clusters=data_args.num_clusters)
@@ -188,7 +199,7 @@ def run_mrc(
             stride=data_args.doc_stride,
             return_overflowing_tokens=True,
             return_offsets_mapping=True,
-            # return_token_type_ids=False, # roberta모델을 사용할 경우 False, bert를 사용할 경우 True로 표기해야합니다.
+            return_token_type_ids=False, # roberta모델을 사용할 경우 False, bert를 사용할 경우 True로 표기해야합니다.
             padding="max_length" if data_args.pad_to_max_length else False,
         )
 
